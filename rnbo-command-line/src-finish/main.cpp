bf7d8248f1274ef23d5a465024e96059b6e0add6
Sam Tarakajian <sam@cycling74.com>
2022-11-09 09:21:54 -0800

    adding the start/finish sources for command line

--- rnbo-command-line/src-finish/main.cpp
+++ rnbo-command-line/src-finish/main.cpp
@@ -13,31 +13,31 @@ using namespace RNBO;
 const uint32_t blocksize = 64;
 
 int main(int argc, const char * argv[]) {
+  // 1. Create the RNBO Core object.
   CoreObject rnboObject;
+
+  // 2. Make a couple of buffers.
   uint32_t sampleBufferSize = 0;
   float *sampleBuffer = nullptr;
   uint32_t sampleOffset = 0;
   float *outputBuffer = nullptr;
 
+  // 3. Read in the input file.
   SF_INFO inputInfo, outputInfo;
   inputInfo.format = 0;
-  SNDFILE *sf = sf_open("anton.aif", SFM_READ, &inputInfo);
+  SNDFILE *sf = sf_open("cherokee.aif", SFM_READ, &inputInfo);
 
-  // Print out some input file info
-  std::cout << "input frames " << inputInfo.frames << "\n";
-  std::cout << "input channels " << inputInfo.channels << "\n";
-
-  // Use the same format to open the output for writing
+  // 4. Use the same format to open the output for writing
   memcpy((void *) &outputInfo, (void *) &inputInfo, sizeof(SF_INFO));
-  SNDFILE *sf_out = sf_open("anton-out.aif", SFM_WRITE, &outputInfo);
+  SNDFILE *sf_out = sf_open("cherokee-out.aif", SFM_WRITE, &outputInfo);
 
-  // Might not be the best idea to read the whole sound file into memory, but
+  // 5. Might not be the best idea to read the whole sound file into memory, but
   // it's fine for a teaching example
+  // Make space to store the file
   if (sf && sf_out) {
-    // Make space to store the file
     sampleBufferSize = sizeof(float) * inputInfo.frames * inputInfo.channels;
     sampleBuffer = (float *) malloc(sampleBufferSize);
-    outputBuffer = (float *) malloc(blocksize * 2 * sizeof(float));
+    outputBuffer = (float *) malloc(blocksize * sizeof(float));
 
     std::cout << "--- Trying to read " << sampleBufferSize << " bytes" << "\n";
     int readSamples = sf_read_float(sf, (float *) sampleBuffer, sampleBufferSize);
@@ -48,44 +48,35 @@ int main(int argc, const char * argv[]) {
     std::cout << "--- Failed" << "\n";
   }
 
-  // Get ready to process stereo audio in blocksize-sample chunks
+  // 6. Get ready to process stereo audio in blocksize-sample chunks
   if (sampleBuffer) {
+
+    // 7. We only need one input and output, since everything is mono
     rnboObject.prepareToProcess(44100, blocksize);
-    RNBO::SampleValue** inputs = new RNBO::SampleValue*[2];
+    RNBO::SampleValue** inputs = new RNBO::SampleValue*[1];
     inputs[0] = new double[blocksize];
-    inputs[1] = new double[blocksize];
-    RNBO::SampleValue** outputs = new RNBO::SampleValue*[2];
+    RNBO::SampleValue** outputs = new RNBO::SampleValue*[1];
     outputs[0] = new double[blocksize];
-    outputs[1] = new double[blocksize];
 
-    int blockcount = 0;
+    // 8. Do all of the audio processing
+    for (uint32_t i = 0; i < sampleBufferSize / (sizeof(float)); i += (blocksize)) {
 
-    // Do all of the audio processing
-    for (uint32_t i = 0; i < sampleBufferSize / (sizeof(float)); i += (2 * blocksize)) {
-      // Read into your input buffers, remembering that the input is interleaved
+      // 9. Read into your input buffers, remembering that the input is interleaved
       for (uint32_t j = 0; j < blocksize; j++) {
-        inputs[0][j] = sampleBuffer[i + 2 * j];
-        inputs[1][j] = sampleBuffer[i + 2 * j + 1];
+        inputs[0][j] = sampleBuffer[i + j];
       }
 
-      // Process the audio
-      rnboObject.process(inputs, 2, outputs, 2, blocksize);
+      // 10. Process the audio
+      rnboObject.process(inputs, 1, outputs, 1, blocksize);
 
-      // Write the output, again mind the interleaving
-      for (uint32_t j = 0; j < blocksize; j++) {
-        outputBuffer[j * 2] = outputs[0][j];
-        outputBuffer[j * 2 + 1] = outputs[1][j];
-      }
-
-      // Ship that buffer to the output file
-      sf_writef_float(sf_out, outputBuffer, blocksize);
+      // 11. Ship that buffer to the output file
+      sf_writef_float(sf_out, outputs[0], blocksize);
     }
 
+    // 12. Cleanup
     delete [] inputs[0];
-    delete [] inputs[1];
     delete [] inputs;
     delete [] outputs[0];
-    delete [] outputs[1];
     delete [] outputs;
     free(sampleBuffer);
     free(outputBuffer);
